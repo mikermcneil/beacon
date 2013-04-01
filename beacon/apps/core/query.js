f9c9bd95fe8e7658ef22f63c6cdca20796ac5cd0
dylan hassinger <d@dylanized.com>
2013-03-31 20:49:36 -0500

    cleaned up beacon stub files

--- beacon/apps/core/query.js
+++ beacon/apps/core/query.js
@@ -1,14 +1,1 @@
-{
-    "name": "beacon",
-    "title": "Beacon"
-    "private": true,
-    "version": "0.0.0",
-    "description": "a better blogging platform",
-    "dependencies": {
-        "sails": "0.8.894",
-        "jam": null,
-		"moment": null        
-    },
-    "author": "",
-    "license": "MIT"
-}
+/* query code goes here */
